880db2c066c91d62f434fe94dbe9a95059aca8f6
Anthony Young <ayoung93@gmail.com>
2014-12-31 17:00:03 +0000

    bodyParser update

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//Dependencies 
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
@@ -15,7 +16,8 @@ mongoose.connect(config.db);
 var app = express();
 
 
-//Tell our app to use
+//Tell our app to use middlewares
+app.use( bodyParser.urlencoded({ extended: true }) );
 app.use(bodyParser.json());
 
 
