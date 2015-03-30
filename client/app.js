68f1389a924b5ad0d406bde86cca249616ddc312
Anthony Young <github@ayoungh.co.uk>
2015-03-30 13:00:42 +0100

    Start on clientside

--- client/app.js
+++ client/app.js
@@ -1,1 +1,28 @@
-angular.module('Hungr');
+//ANGULARJS
+(function(){
+	'use strict';
+
+	//define app
+	var app = angular.module('Hungr', [], function config($httpProvider) {
+		//$httpProvider.interceptors.push('AuthInteceptor');
+	});
+
+	//constants
+	app.constant('API_URL', 'http://localhost:3000');
+	app.constant('AppName', 'Hungr');
+
+
+	//Controllers
+	app.controller('MainCtrl', function MainCtrl(AppName){
+		'use strict';
+		var vm = this;
+		vm.AppName = AppName;
+
+	});
+
+
+
+
+
+
+})();
